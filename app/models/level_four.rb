16f9d0694324680b6b466bd27e4c1e7343617918
Prelang Builder <builder@prelang.com>
2015-12-20 17:10:20 +0000

    Generated model LevelFour with migration

---
+++ app/models/level_four.rb
@@ -0,0 +1,2 @@
+class LevelFour < ActiveRecord::Base
+end
---
+++ db/migrate/20151220171020_create_level_fours.rb
@@ -0,0 +1,8 @@
+class CreateLevelFours < ActiveRecord::Migration
+  def change
+    create_table :level_fours do |t|
+
+      t.timestamps
+    end
+  end
+end
